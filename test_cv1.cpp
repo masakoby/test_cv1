2a6e25d8d730b3d6d77e3e553ff406f4f4f30f23
Masaharu Kobayashi <masakoby@gmail.com>
2021-12-07 14:49:17 +0900

    add file

---
+++ test_cv1.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+    cv::Mat mat1 = cv::imread(argv[1]);
+
+    cv::namedWindow("image", cv::WINDOW_NORMAL);
+    cv::imshow("image", mat1);
+    cv::waitKey(0);
+
+    return 0;
+}
+
